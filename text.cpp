96ac27ff6f8546ef52584b083d8569c16aecce87
Janghan Park <parkjanghan2256@naver.com>
2025-01-25 22:22:02 +0900

    boj problem number : 6064

--- text.cpp
+++ text.cpp
@@ -5,28 +5,71 @@
 
 using namespace std;
 
+
+
 int main() {
-    int N, M;
-    cin >> N >> M;
-    
-    vector<int> a(N);
-    int closest = 0;
+    int n, m;
+    cin >> n >> m;
 
-    for (vector<int>::iterator it = a.begin(); it < a.end(); it++){
-        cin >> *it;
-    }
-
-    for (int i = 0; i < N; i++){
-        for (int j = 0; j < i; j++){
-            for (int k = 0; k < j; k++){
-                if (a[i]+a[j]+a[k] <= M && a[i]+a[j]+a[k] > closest ){
-                    closest = a[i]+a[j]+a[k];
-                }
-                //cout << a[i] << " " << a[j] << " " << a[k] << "\n" ;
-            }
+    //1. input
+    vector<vector<int>> a(n+1,vector<int> (m+1));    
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= m; j++){
+            cin >> a[i][j];
         }
     }
 
-    cout << closest;
+    int sumMax = 0;
+    int count = 0;
+
+    //2. check type-1
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j+3 <= m; j++){
+            sumMax = max(a[i][j] + a[i][j+1] + a[i][j+2] + a[i][j+3], sumMax); 
+            count++;
+        }
+    }
+    for (int i = 1; i+3 <= n; i++){
+        for (int j = 1; j <= m; j++){
+            sumMax = max(a[i][j] + a[i+1][j] + a[i+2][j] + a[i+3][j], sumMax); 
+            count++;
+        }
+    }
+
+    //3. check type-2
+    for (int i = 1; i+1 <= n; i++){
+        for (int j = 1; j+1 <= m; j++){
+            sumMax = max(a[i][j] + a[i][j+1] + a[i+1][j] + a[i+1][j+1], sumMax); 
+            count++;
+        }
+    }
+
+    //4. check type-3 (L shape)
+    for (int i = 1; i+2 <= n; i++){
+        for (int j = 1; j+1 <= m; j++){
+            for (int k =0; k<3; k++){
+                sumMax = max(a[i][j] + a[i+1][j] + a[i+2][j] + a[i+k][j+1], sumMax); 
+                sumMax = max(a[i][j+1] + a[i+1][j+1] + a[i+2][j+1] + a[i+k][j], sumMax);
+            }
+            sumMax = max(a[i][j] + a[i+1][j] + a[i+1][j+1] + a[i+2][j+1], sumMax); 
+            sumMax = max(a[i][j+1] + a[i+1][j] + a[i+1][j+1] + a[i+2][j], sumMax);
+            count++;
+        }
+    }
+
+    for (int i = 1; i+1 <= n; i++){
+        for (int j = 1; j+2 <= m; j++){
+            for (int k =0; k<3; k++){
+                sumMax = max(a[i][j] + a[i][j+1] + a[i][j+2] + a[i+1][j+k], sumMax); 
+                sumMax = max(a[i+1][j] + a[i+1][j+1] + a[i+1][j+2] + a[i][j+k], sumMax);
+            }
+            sumMax = max(a[i][j] + a[i][j+1] + a[i+1][j+1] + a[i+1][j+2], sumMax); 
+            sumMax = max(a[i+1][j] + a[i][j+1] + a[i+1][j+1] + a[i][j+2], sumMax);
+            count++;
+        }
+    }
+
+    cout << sumMax ;
+
 }
 
