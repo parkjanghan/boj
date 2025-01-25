812416821c30ef475aed2cb63e71c2aa670717fb
Janghan Park <parkjanghan2256@naver.com>
2025-01-25 20:28:24 +0900

    백준 2798번

--- text.cpp
+++ text.cpp
@@ -6,6 +6,27 @@
 using namespace std;
 
 int main() {
-    //백준 문제 번호 업데이트
+    int N, M;
+    cin >> N >> M;
+    
+    vector<int> a(N);
+    int closest = 0;
+
+    for (vector<int>::iterator it = a.begin(); it < a.end(); it++){
+        cin >> *it;
+    }
+
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < i; j++){
+            for (int k = 0; k < j; k++){
+                if (a[i]+a[j]+a[k] <= M && a[i]+a[j]+a[k] > closest ){
+                    closest = a[i]+a[j]+a[k];
+                }
+                //cout << a[i] << " " << a[j] << " " << a[k] << "\n" ;
+            }
+        }
+    }
+
+    cout << closest;
 }
 
